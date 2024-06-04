4ad0dfbfb88d59d4edf70dc4a018ff4d4142a185
Aleksandr Tarelkin <tarelkinaa@bk.ru>
2024-06-04 14:22:32 +0300

    check deposit event

--- tests/mainnet/pre-release/All-round-happy-path.t.sol
+++ tests/mainnet/pre-release/All-round-happy-path.t.sol
@@ -22,15 +22,19 @@ contract AllRoundHappyPath is TestHelpers {
     function testFork_BaseWstethDepositWithdrawalWithRabase() external {
         DeploymentConfiguration memory config = deploymentConfigurations[0];
 
+        address[] memory users = new address[](1);
+        users[0] = DEPOSITOR;
+
         // Deposit
         vm.startPrank(DEPOSITOR);
-        // IERC20(address(_WSTETH)).approve(address(config.vault), depositAmount);
         IERC20(address(_WSTETH)).approve(address(config.depositWrapper), depositAmount);
         uint256[] memory amounts = new uint256[](1);
         amounts[0] = depositAmount;
 
         uint256 depositorWstethBalanceBefore = IERC20(address(_WSTETH)).balanceOf(DEPOSITOR);
-        // config.vault.deposit(DEPOSITOR, amounts, depositAmount, type(uint256).max);
+
+        vm.expectEmit(true, true, false, true, address(config.vault));
+        emit IVault.Deposit(DEPOSITOR, amounts, 116861303836991156599); // TODO: check lp amount
         config.depositWrapper.deposit(DEPOSITOR, address(_WSTETH), depositAmount, 0, type(uint256).max);
         uint256 depositorWstethBalanceAfterDeposit = IERC20(address(_WSTETH)).balanceOf(DEPOSITOR);
 
@@ -51,8 +55,6 @@ contract AllRoundHappyPath is TestHelpers {
         vm.stopPrank();
 
         // Admin approve
-        address[] memory users = new address[](1);
-        users[0] = DEPOSITOR;
         vm.prank(config.admin);
         config.defaultBondStrategy.processAll();
 
