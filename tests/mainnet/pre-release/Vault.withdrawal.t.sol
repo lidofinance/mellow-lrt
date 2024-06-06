f9996bb21ed3aadf79d30e03360934f64620e202
Alexey Potapkin <folkyatina@gmail.com>
2024-06-06 19:00:18 +0300

    test(withdrawals): 1200 depositors

--- tests/mainnet/pre-release/Vault.withdrawal.t.sol
+++ tests/mainnet/pre-release/Vault.withdrawal.t.sol
@@ -27,18 +27,20 @@ contract WithdrawalSteakhouse is TestHelpers {
         bondContractBalance = _WSTETH.balanceOf(CONFIG().wstethDefaultBond);
         vault = Vault(CONFIG().vault);
 
-        depositors = new address[](3);
-        lpAmounts = new uint256[](3);
-        deposits = new uint256[](3);
+        uint256 num = 1200;
 
-        depositors[0] = address(bytes20(keccak256("depositorX")));
-        depositors[1] = address(bytes20(keccak256("depositorY")));
-        depositors[2] = address(bytes20(keccak256("depositorZ")));
-
-        uint256 amount = 10 ether;
+        depositors = new address[](num);
+        lpAmounts = new uint256[](num);
+        deposits = new uint256[](num);
 
         for (uint256 i = 0; i < depositors.length; ++i) {
-            deposits[i] = assignWstETH(depositors[i], amount * (i + 1));
+            depositors[i] = address(uint160(uint256(keccak256("depositor")) + i));
+        }
+
+        uint256 amount = 0.01 ether;
+
+        for (uint256 i = 0; i < depositors.length; ++i) {
+            deposits[i] = assignWstETH(depositors[i], amount + 0.001 ether);
             lpAmounts[i] = vault.deposit(depositors[i], deposits[i]);
         }
     }
@@ -47,7 +49,7 @@ contract WithdrawalSteakhouse is TestHelpers {
         DeploymentConfiguration memory config = CONFIG();
         assertEq(_WSTETH.balanceOf(config.wstethDefaultBond) - bondContractBalance, sum(deposits));
         (, uint256[] memory amounts) = vault.underlyingTvl();
-        assertApproxEqAbs(sum(deposits), sum(lpAmounts) * amounts[0] / vault.totalSupply(), 1);
+        assertEqOrLess(sum(lpAmounts) * amounts[0] / vault.totalSupply(), sum(deposits), deposits.length);
 
         assertEq(vault.balanceOf(depositors[0]), lpAmounts[0]);
         assertEq(vault.balanceOf(depositors[1]), lpAmounts[1]);
@@ -64,8 +66,6 @@ contract WithdrawalSteakhouse is TestHelpers {
 
         vault.withdrawal(depositor, lpAmount, depositWstethAmount - 1); //! dust
 
-        rebase(10);
-
         {
             address[] memory withdrawers = vault.pendingWithdrawers();
             assertEq(withdrawers.length, withdrawersLength + 1);
@@ -79,7 +79,7 @@ contract WithdrawalSteakhouse is TestHelpers {
             );
         assertTrue(isProcessingPossible);
         assertFalse(isWithdrawalsPossible);
-        assertApproxEqAbs(expectedAmounts[0], depositWstethAmount, 1);
+        assertEqOrLess(expectedAmounts[0], depositWstethAmount, 1);
 
         vm.startPrank(config.curator);
         {
@@ -90,8 +90,8 @@ contract WithdrawalSteakhouse is TestHelpers {
             address[] memory withdrawers = vault.pendingWithdrawers();
             assertEq(withdrawers.length, withdrawersLength);
 
-            assertApproxEqAbs(_WSTETH.balanceOf(depositor), depositWstethAmount, 1);
-            assertApproxEqAbs(_WSTETH.balanceOf(config.wstethDefaultBond), bondContractBalance + deposits[1] + deposits[2], 1);
+            assertEqOrLess(_WSTETH.balanceOf(depositor), depositWstethAmount, 1);
+            assertEqOrLess(bondContractBalance + sum(deposits) - deposits[0], _WSTETH.balanceOf(config.wstethDefaultBond), 1);
         }
         vm.stopPrank();
     }
@@ -101,17 +101,15 @@ contract WithdrawalSteakhouse is TestHelpers {
         uint256 withdrawersLength = vault.pendingWithdrawers().length;
 
         for (uint256 i = 0; i < depositors.length; ++i) {
-             vault.withdrawal(depositors[i], lpAmounts[i], deposits[i] - 2); //! dust intensyfies
+             vault.withdrawal(depositors[i], lpAmounts[i], deposits[i] - 3); //! dust intensyfies
         }
-
-        rebase(10);
 
         {
             address[] memory withdrawers = vault.pendingWithdrawers();
-            assertEq(withdrawers.length, withdrawersLength + 3);
-            assertEq(withdrawers[withdrawers.length - 1], depositors[2]);
-            assertEq(withdrawers[withdrawers.length - 2], depositors[1]);
-            assertEq(withdrawers[withdrawers.length - 3], depositors[0]);
+            assertEq(withdrawers.length, withdrawersLength + deposits.length);
+            for (uint256 i = 0; i < depositors.length; ++i) {
+                assertEq(withdrawers[withdrawers.length - 1 - i], depositors[depositors.length - 1 - i]);
+            }
         }
 
         IVault.ProcessWithdrawalsStack memory stack = vault.calculateStack();
@@ -124,7 +122,7 @@ contract WithdrawalSteakhouse is TestHelpers {
                 );
             assertTrue(isProcessingPossible);
             assertFalse(isWithdrawalsPossible);
-            assertApproxEqAbs(expectedAmounts[0], deposits[i], 2); // !wtf it is 2
+            assertEqOrLess(expectedAmounts[0], deposits[i], 3); // !wtf it is 2
         }
 
         vm.startPrank(config.curator);
@@ -136,12 +134,12 @@ contract WithdrawalSteakhouse is TestHelpers {
             config.defaultBondStrategy.processWithdrawals(users);
 
             address[] memory withdrawers = vault.pendingWithdrawers();
-            assertEq(withdrawers.length, withdrawersLength + (2 - i));
+            assertEq(withdrawers.length, withdrawersLength + (depositors.length - 1 - i));
 
             bondContractBalanceAfter -= deposits[i];
 
-            assertApproxEqAbs(_WSTETH.balanceOf(depositors[i]), deposits[i], 1);
-            assertApproxEqAbs(_WSTETH.balanceOf(config.wstethDefaultBond), bondContractBalanceAfter, deposits.length);
+            assertApproxEqAbs(_WSTETH.balanceOf(depositors[i]), deposits[i], 4);
+            assertEqOrLess(bondContractBalanceAfter, _WSTETH.balanceOf(config.wstethDefaultBond), deposits.length * 4);
         }
         vm.stopPrank();
     }
@@ -151,10 +149,21 @@ contract WithdrawalSteakhouse is TestHelpers {
         uint256 withdrawersLength = vault.pendingWithdrawers().length;
 
         for (uint256 i = 0; i < depositors.length; ++i) {
-             vault.withdrawal(depositors[i], lpAmounts[i], deposits[i] - 2); //! dust intensyfies
+             vault.withdrawal(depositors[i], lpAmounts[i], deposits[i] - 3); //! dust intensyfies
         }
 
-        rebase(10);
+        IVault.ProcessWithdrawalsStack memory stack = vault.calculateStack();
+
+        for (uint256 i = 0; i < depositors.length; ++i) {
+            (bool isProcessingPossible, bool isWithdrawalsPossible, uint256[] memory expectedAmounts) = vault
+                .analyzeRequest(
+                    stack,
+                    vault.withdrawalRequest(depositors[i])
+                );
+            assertTrue(isProcessingPossible);
+            assertFalse(isWithdrawalsPossible);
+            assertEqOrLess(expectedAmounts[0], deposits[i], 3); // !wtf it is 2
+        }
 
         vm.startPrank(config.curator);
         uint256 bondContractBalanceAfter = bondContractBalance + sum(deposits);
@@ -168,11 +177,17 @@ contract WithdrawalSteakhouse is TestHelpers {
 
             bondContractBalanceAfter -= deposits[i - 1];
 
-            assertApproxEqAbs(_WSTETH.balanceOf(depositors[i-1]), deposits[i-1], 2);
-            assertApproxEqAbs(_WSTETH.balanceOf(config.wstethDefaultBond), bondContractBalanceAfter, deposits.length);
+            assertApproxEqAbs(_WSTETH.balanceOf(depositors[i-1]), deposits[i-1], 4);
+            assertEqOrLess(bondContractBalanceAfter, _WSTETH.balanceOf(config.wstethDefaultBond), deposits.length*4);
         }
         vm.stopPrank();
     }
+
+    function assertEqOrLess(uint256 a, uint256 b, uint256 delta) public pure {
+        vm.assertTrue(b >= a && b - a <= delta,
+            string.concat("asssertEqOrLess: should be ", vm.toString(a), "<=", vm.toString(b)));
+    }
+
 }
 
 contract WithdrawalRe7 is WithdrawalSteakhouse {
